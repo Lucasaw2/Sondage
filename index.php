cdd181f14be4ef63807ee2fb1b2454241a08dc4e
LXM <53872250+Lucasaw2@users.noreply.github.com>
2022-10-05 10:31:18 +0200

    Mise en place de la base du Sondage
    Ajout des fichier du github de test

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Script PHP crée par LXM
+////////////////////////////////
+// Systèmes de suppression du cache (pas fiables a 100%)
+header("Cache-Control:no-cache");
+// Affichage de la vue Entête
+include "vues/v_entete_strawpoll.html";
+// Affichage de la vue Body
+include "vues/v_body_strawpoll.php";
+//include "vues/v_loader.php"
+?>
---
+++ vues/v_body_strawpoll.php
@@ -0,0 +1,41 @@
+<body>
+    <section class="section_menu">
+        <header class="select">
+            <a href="https://asci-asso.fr/accueil-asci/" class="titre">ASCI-Sondage</a>
+            <ul>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#" class="active">BTS SIO 1</a></li>
+                <li><a href="#">BTS SIO 2</a></li>
+            </ul>
+        </header>
+        <h1 id="title">Sondage journée d'intégration</h1>
+        <form>
+            <h1>Formulaire - Repas partagée</h1>
+            <div class="separation"></div>
+            <div class="corps-formulaire">
+                <div class="gauche">
+                    <div class="groupe">
+                        <label>Votre Nom</label>
+                        <input type="text" autocomplete="off" />
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="groupe">
+                        <label>Votre adresse e-mail</label>
+                        <input type="text" autocomplete="off" />
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                </div>
+                <div class="droite">
+                    <div class="groupe">
+                        <label>Que ramenez vous ?</label>
+                        <textarea placeholder="Saisissez ici..."></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="pied-formulaire" align="center">
+                <button>Envoyer le formulaire</button>
+            </div>
+        </form>
+        <p id="pub-asci"><em><a href="https://asci-asso.fr/accueil-asci/">Inscrivez vous à ASCI en cliquant  sur ce texte</a></em></p>
+    </section>
+</body>
