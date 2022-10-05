1f141ca7e0e188096d3d1f117e7c68f1db2df5a4
Lucasaw6 <wollluc@gmail.com>
2022-10-05 23:01:37 +0200

    Mise en place de l'insert pour database

---
+++ database/Sondage_DATABASE_insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO classe(Id_classe , nom)
+VALUES (NULL, 'BTS SIO 1'),
+(NULL, 'BTS SIO 2');
+
+INSERT INTO eleves (Id_eleves, nom, prenom, mail, alergie, Id_classe)
+VALUES (NULL, 'ARNAUD--WOLLBRETT', 'Lucas', 'lucas_arnaudwollbrett@outlook.fr', NULL, 1),
+(NULL, 'BENOIT', 'Dylan', 'dylan_benoit@outlook.fr', NULL, 1),
+(NULL, 'BOURIN', 'Jules', 'julesbourin@outlook.fr', NULL, 1),
+(NULL, 'CHAILAN', 'Bérénice', 'berenice.chailan@outlook.fr', NULL, 1),
+(NULL, 'DE CIANCIO', 'Enzo', 'enzo.deciancio@outlook.fr', NULL, 1),
+(NULL, 'DEPOITRE', 'Antoine', 'antoine.depoitre@outlook.fr', NULL, 1),
+(NULL, 'DJELASSI', 'Lenny', 'lennydjelassi@outlook.fr', NULL, 1),
+(NULL, 'DURAND', 'Loic', 'loic_durand@outlook.fr', NULL, 1),
+(NULL, 'EK', 'Romain', 'romain.ek@outlook.fr', NULL, 1),
+(NULL, 'FALLOT', 'Théo', 'theo_fallot@outlook.fr', NULL, 1),
+(NULL, 'FAUCHER', 'Maxence', 'maxencefaucher@outlook.fr', NULL, 1),
+(NULL, 'FETTAR', 'Mohamed', 'mohamed.fettar@outlook.fr', NULL, 1),
+(NULL, 'GAYRBEKOV', 'Viskhan', 'viskhan.gayrbekov@hotmail.com', NULL, 1),
+(NULL, 'GOMES', 'Léo', 'leo-gomes@outlook.fr', NULL, 1),
+(NULL, 'GUZEL', 'Gunay', 'gunay.guzel@hotmail.com', NULL, 1),
+(NULL, 'LADEL', 'Milan', 'm_ladel@outlook.fr', NULL, 1),
+(NULL, 'LEHAF', 'Moustapha', 'moustapha.lehaf@outlook.fr', NULL, 1),
+(NULL, 'MAURY', 'Damien', 'd.maury@hotmail.com', NULL, 1),
+(NULL, 'MONTENAT DE FAYS', 'Chloé', 'chloemontenat.defays@outlook.fr', NULL, 1),
+(NULL, 'MUSLIMOV', 'Muslim', 'muslim.muslimov@outlook.fr', NULL, 1),
+(NULL, 'PASQUIN', 'Angelo', 'angelo.pasquin@outlook.fr', NULL, 1),
+(NULL, 'PETERMANN', 'Lukas', 'lukas.petermann@outlook.fr', NULL, 1),
+(NULL, 'SCHNELZAUER', 'Lucas', 'lucas.schnelzauer@outlook.fr', NULL, 1),
+(NULL, 'STRUB', 'Noah', 'noah_strub@outlook.fr', NULL, 1),
+(NULL, 'TARHINI', 'William', 'william.tarhini@outlook.fr', NULL, 1),
+(NULL, 'THUILLIER', 'Clément', 'clement.thuillier@outlook.fr', NULL, 1),
+(NULL, 'VOSGIEN', 'Augustin', 'augustin-vosgien@hotmail.com', NULL, 1),
+(NULL, 'ZEKRI', 'Mehdi', 'mehdi_zekri@outlook.com', NULL, 1),
+(NULL, 'AUBERTIN', 'Bryan', 'aubertin.b@hotmail.com', NULL, 2),
+(NULL, 'BENBARKA', 'Elias', 'elias.benbarka@gmail.com', NULL, 2),
+(NULL, 'CHERRIÈRE', 'Yoan', 'yoan.cherriere.bts@gmail.com', NULL, 2),
+(NULL, 'CLEMENT', 'Thomas', 'clemthom6@gmail.com', NULL, 2),
+(NULL, 'DERREZ', 'Elisa', 'drzelisa@gmail.com', NULL, 2),
+(NULL, 'DERVAUX', 'Maxime', 'maximedervaux51@gmail.com', NULL, 2),
+(NULL, 'EL MESKIRI', 'Brian', 'brian.elmeskiri@icloud.com', NULL, 2),
+(NULL, 'FETHALLAH', 'Solaimane', 'solaimane.fethallah@gmail.com', NULL, 2),
+(NULL, 'GARNIER', 'Ronan', 'ronangarnier11@gmail.com', NULL, 2),
+(NULL, 'GIULIANA', 'Nino', 'nino.giuliana54@gmail.com', NULL, 2),
+(NULL, 'HANEN', 'Anthony', 'ha.anthony@outlook.fr', NULL, 2),
+(NULL, 'HENQUEZ', 'Enzo', 'enzo.henquez@orange.fr ', NULL, 2),
+(NULL, 'HOFFMANN', 'Theo', 'theohoffmann54400@gmail.com', NULL, 2),
+(NULL, 'KUNSUNGA MUHIKA', 'David', 'davidkunsunga@gmail.com', NULL, 2),
+(NULL, 'MAIRE', 'Timothee', 'timothee.maire54300@gmail.com', NULL, 2),
+(NULL, 'MALTIN', 'Jean-François', 'jean_francois.maltin@outlook.fr', NULL, 2),
+(NULL, 'MARTIN', 'Julian', 'julian88martin@gmail.com', NULL, 2),
+(NULL, 'MAXIMIN-TARTARE', 'David', 'maximin.david54@gmail.com', NULL, 2),
+(NULL, 'NABHIH EL ALAOUI', 'Mehdi', 'mehdinea1@gmail.com', NULL, 2),
+(NULL, 'OLIVEIRA', 'Mael', 'm.oliveira88170@gmail.com', NULL, 2),
+(NULL, 'OUILI', 'Amine', 'amine.ouili@outlook.fr', NULL, 2),
+(NULL, 'OZDEMIR', 'Senol', 'senol-ozdemir@outlook.fr', NULL, 2),
+(NULL, 'PICART', 'Loïc', 'loic.picart1@outlook.fr', NULL, 2),
+(NULL, 'ROSEMAIN', 'James', 'jamesrosemainn@gmail.com', NULL, 2),
+(NULL, 'THOUVENOT', 'Lucas', 'lucasthouvenot329@gmail.com', NULL, 2);
+
+
+
+
+
+
+
+
+
