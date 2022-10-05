8805fdee14cde72bae0a013562a385275d475c50
LXM <53872250+Lucasaw2@users.noreply.github.com>
2022-10-05 13:51:59 +0200

    Test pour multiple pages

--- vues/v_body_strawpoll.php
+++ vues/v_body_strawpoll.php
@@ -1,13 +1,13 @@
 <body>
-    <section class="section_menu">
-        <header class="select">
-            <a href="https://asci-asso.fr/accueil-asci/" class="titre">ASCI-Sondage</a>
-            <ul>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#" class="active">Sondage</a></li>
-                <li><a href="#">Résultat</a></li>
-            </ul>
-        </header>
+    <header class="select">
+        <a href="https://asci-asso.fr/accueil-asci/" class="titre">ASCI-Sondage</a>
+        <ul>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#" class="active">Sondage</a></li>
+            <li><a href="#">Résultat</a></li>
+        </ul>
+    </header>
+    <section class="section_sondage">
         <h1 id="title">Sondage journée d'intégration</h1>
         <form>
             <h1>Formulaire - Repas partagée</h1>
@@ -36,6 +36,10 @@
                 <button>Envoyer le formulaire</button>
             </div>
         </form>
+        <p id="pub-asci"><em><a href="https://asci-asso.fr/accueil-asci/">Inscrivez vous à ASCI en cliquant  sur ce texte</a></em></p>
+    </section>
+    <section class="section_menu" id="hide">
+        <h1 id="title">Vous êtes sur le menu</h1>
         <p id="pub-asci"><em><a href="https://asci-asso.fr/accueil-asci/">Inscrivez vous à ASCI en cliquant  sur ce texte</a></em></p>
     </section>
 </body>
